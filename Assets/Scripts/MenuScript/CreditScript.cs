8536c4412b5e0844e5dce8055ac6f8086ec15710
stuartbaillie69 <60524718+stuartbaillie69@users.noreply.github.com>
2024-01-30 21:08:57 +0000

    Credits Menu Positions
    added start and end position based on screen size

--- Assets/Scripts/MenuScript/CreditScript.cs
+++ Assets/Scripts/MenuScript/CreditScript.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreditScript : MonoBehaviour
 {
     public GameObject gm;
     // Start is called before the first frame update
+    private float startPos;
+    private float endPos;
+    private float creditsHeight;
+
     void Start()
     {
-    }
+        Debug.Log("Screen Height : " + Screen.height);
+        creditsHeight = this.GetComponent<RectTransform>().rect.height;
+        Debug.Log("Height " + creditsHeight);
 
-    // Update is called once per frame
+        startPos = (((Screen.height/2) + (creditsHeight/2))); //  add on the length of the credits object
+        Debug.Log("start pos " + startPos);
+        
+        endPos = (Screen.height / 2);
+//        transform.position = new Vector3(transform.position.x, startPos);
+        Debug.Log("pos " + transform.position);
+
+        transform.position = new Vector3(transform.position.x, transform.position.y - startPos);
+        Debug.Log("pos " + transform.position);
+
+    }
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y + 20f * Time.deltaTime);
-        if (transform.position.y > 800)
+        transform.position = new Vector3(transform.position.x, transform.position.y + 30f * Time.deltaTime);
+        if (transform.position.y > startPos)
         {
             gm.GetComponent<ExitMenusScript>().ExitCredits();
         }
